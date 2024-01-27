ae3ca2a5e26b8c191621facf7fb7a4b9d9068006
elgaricito <edgarjaguirre@hotmail.com>
2024-01-27 18:04:07 -0300

    Chapter 14 WebDriver Extras finished

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -5,6 +5,7 @@ import com.seleniumPractice.utils.EventReporter;
 import com.seleniumPractice.utils.WindowManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
@@ -12,6 +13,7 @@ import org.testng.annotations.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -25,7 +27,7 @@ public class BaseTest {
 
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         //driver = new ChromeDriver();
-        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver = new EventFiringWebDriver(new ChromeDriver(getChromeOptions()));
         driver.register(new EventReporter());
         //wait amount of time to a page to load until complete before throw an error
         //driver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
@@ -33,16 +35,18 @@ public class BaseTest {
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         goHome();
         //driver.manage().window().setSize(new Dimension(375,812));
-        homePage=new HomePage(driver);
+        //homePage=new HomePage(driver);
 
-        List<WebElement> webElements= driver.findElements(By.tagName("a"));
+        //List<WebElement> webElements= driver.findElements(By.tagName("a"));
         //webElements.forEach(x-> System.out.println(x.getText()));
+        setCookie();
 
     }
 
     @BeforeMethod
     public void goHome(){
         driver.get("https://the-internet.herokuapp.com/");
+        homePage=new HomePage(driver);
     }
 
     @AfterClass
@@ -86,6 +90,21 @@ public class BaseTest {
     }
     public WindowManager getWindowManager(){
         return new WindowManager(driver);
+    }
+
+    private ChromeOptions getChromeOptions(){
+        ChromeOptions options= new ChromeOptions();
+        //options.addArguments("disable-infobars");
+        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+        options.setExperimentalOption("useAutomationExtension", false);
+        return options;
+    }
+
+    private void setCookie(){
+        Cookie cookie=new Cookie.Builder("Tau","123")
+                .domain("the-internet.herokuapp.com")
+                .build();
+        driver.manage().addCookie(cookie);
     }
 
 }
